ec9d68d9e9d8f35345a8aca4698e372031654d72
Jugg <letatanu@gmail.com>
2020-03-13 20:10:12 -0700

    Updates

---
+++ precache-manifest.8b86d5414fa68f639d73eaa76c61066a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bb71bb59dc1f6d9fe238bfa4103204",
+    "url": "/FinalProject/index.html"
+  },
+  {
+    "revision": "21440b09ed03670de79c",
+    "url": "/FinalProject/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fe5078d8692879322b7f",
+    "url": "/FinalProject/static/css/main.e14e350c.chunk.css"
+  },
+  {
+    "revision": "21440b09ed03670de79c",
+    "url": "/FinalProject/static/js/2.3154f4c6.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/FinalProject/static/js/2.3154f4c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5078d8692879322b7f",
+    "url": "/FinalProject/static/js/main.07476026.chunk.js"
+  },
+  {
+    "revision": "7c6d7370644f6e698a42",
+    "url": "/FinalProject/static/js/runtime-main.3edd11f7.js"
+  }
+]);
